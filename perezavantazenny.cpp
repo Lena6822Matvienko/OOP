3225f9d309a7d67c9959826aa8ffa4e7927c23aa
Lena6822Matvienko <elena6822@mail.ru>
2014-05-13 17:57:54 +0200

    Update perezavantazenny.cpp

--- perezavantazenny.cpp
+++ perezavantazenny.cpp
@@ -1,55 +1,76 @@
+#include <cstdlib>
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-
+#include <math.h>
 
 using namespace std;
-
 class complex
 {
-    protected:
-	double x; 
-	double re;
-	double im;
-	public:
-	complex(double RE, double IM);
-	complex();
-	complex operator-();
-	complex operator=(complex &b)
-	{
-		re=b.re;
-		im=b.im;
-	}
-	void show();
-};
-complex::complex()
-{
-	re=0;
-	im=0;
-}
-complex::complex(double RE, double IM)
-{
-	re=RE;
-	im=IM;
+      protected:
+                double re;
+                double im;
+      public: 
+      complex(double RE, double IM);
+      ~complex();
+      complex operator-();
+      complex operator+(complex &b);
+      double module();
+      void show();
+ 
+      
+      complex operator=(complex &b)
+      {
+              re=b.re;
+              im=b.im;
+      }
+ }; 
+      
+      complex::complex(double RE, double IM)
+      {
+         re=RE;
+         im=IM;
+      }
+  complex::~complex()
+  {               
+  }
+  complex complex::operator-()
+  { 
+          complex x(-re, -im);
+           return x;
+   }
+   void complex:: show()
+   {
+     if (im>0)
+     cout <<re<<"+"<<im<<"*i";
+     else
+     cout<<re<<im<<"*i";
+   }
+        complex complex::operator+ (complex &b)
+   {
+	  complex x(re, im);
+
+	  x.re = re + b.re;
+	  x.im = im + b.im;
+    
+	  return x;
+   }
+       
+double complex::module()
+{   double i= sqrt (re * re + im * im);
+    return i;
 }
 
-complex complex::operator-()
-{
-	complex x(-re, -im);
-	return x;
-}
-void complex::show()
-{
-	if(im>=0)
-	cout << re << "+" << im << "*i"<< endl;
-	else
-	cout << re<< im << "*i" << endl;
-}
+            
+int main(int argc, char *argv[])
+{ complex a(8,1);
+ complex b(5,1);
+ cout<<endl;
+ (-a).show();
+ cout<<endl;
+ (a).show();
+ cout<<endl;
+ cout << "Module of i1:" << a.module() << '\n';
+ cout << "Module of i2:" << b.module() << '\n';
 
-int main()
-{
-	complex a(1,1);
-	complex b; // b=-a
-	(-a).show();//b.show()
-
+    system("PAUSE");
+    return EXIT_SUCCESS;
 }
